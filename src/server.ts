05cc6bf42ac4ab068f0102df18d69012ae5453fa
Glauco Torres <glaucotrodrigues@gmail.com>
2020-12-01 11:13:40 -0300

    :sparkles: Challenge Finished

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,14 @@
+// import express from 'express';
+// import routes from './routes';
+// import './database';
+
 import app from './app';
+// // import app from './app';
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
